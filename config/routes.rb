d16fde4bb6512f4897c8f7f1d1a556383f349d85
Jacques Nalletamby <jacknalletamby@gmail.com>
2018-08-29 13:29:48 +0200

    5.1 redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
+  resources :users, except: [:index]
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  root 'simple_pages#landing_page'
+  root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
