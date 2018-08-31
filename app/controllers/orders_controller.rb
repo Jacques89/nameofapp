3ecfeb2e0a395b69d8cb6b4ebb65775ae6bf39ee
Jacques Nalletamby <jacknalletamby@gmail.com>
2018-08-31 15:33:44 +0200

    5.4

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  # Login required for orders-access
+  before_action :authenticate_user!
+
    def index
      @orders = Order.includes(:product).all
    end
