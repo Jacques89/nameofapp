ac72db383fdc78017911d25d8df06944e7f9bd41
Ben Bagley <ben@benbagley.co.uk>
2018-09-25 15:22:34 +0100

    fixed price in products table and also orders

--- db/migrate/20180924130041_add_fields_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldsToProducts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :products, :price, :decimal
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_130041) do
+ActiveRecord::Schema.define(version: 2018_09_25_135955) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2018_09_24_130041) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
-    t.decimal "price"
+    t.integer "price"
   end
 
   create_table "users", force: :cascade do |t|
