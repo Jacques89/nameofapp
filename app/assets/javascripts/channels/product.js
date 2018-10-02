425ccbc2ddf9d69c13ac15627b2f724f4f227b2e
Jacques Nalletamby <jacknalletamby@gmail.com>
2018-10-02 12:42:53 +0200

    6.13 Real time updates

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,7 +1,6 @@
 App.product = App.cable.subscriptions.create("ProductChannel", {
   connected: function() {
     // Called when the subscription is ready for use on the server
-    $(".alert.alert-info").show();
   },
    disconnected: function() {
     // Called when the subscription has been terminated by the server
