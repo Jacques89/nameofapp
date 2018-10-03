871414bc7692a7583db44d6300be3f50b6c84300
Jacques Nalletamby <jacknalletamby@gmail.com>
2018-10-04 00:22:20 +0200

    edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,29 @@
 class CommentsController < ApplicationController
-  
-  def create
-      @product = Product.find(params[:product_id])
-      @comment = @product.comments.new(comment_params)
-      @comment.user = current_user
-      @user = current_user
+    def create
+    @product = Product.find(params[:product_id])
+    @comment = @product.comments.new(comment_params)
+    @comment.user = current_user
+    @user = current_user
+    
+   respond_to do |format|
+    if @comment.save
+      ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+      format.html { redirect_to @product, notice: 'Review was created successfully.' }
+      format.json { render :show, status: :created, location: @product }
+      format.js
+    else
+      format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+      format.json { render json: @comment.errors, status: :unprocessable_entity }
+     end
+   end
+ end
 
-    respond_to do |format|
-      if @comment.save
-        ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
-        format.html { redirect_to @product, notice: 'Review was created successfully.' }
-        format.json { render :show, status: :created, location: @product }
-        format.js
-     else
-       format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-       format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+   def destroy
+   end
+
+ private
+
+   def comment_params
+     params.require(:comment).permit(:body, :rating)
+   end
+ end
